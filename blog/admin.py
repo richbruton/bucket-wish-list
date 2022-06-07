ae7287d39dc3114dc025fc1ce7f984d44ebdaca6
richbruton <richbruton@gmail.com>
2022-06-07 18:39:42 +0000

    creatte and complete admin dashboard, and allow for posts and comments to be approved

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('approved', 'created_on')
     summernote_fields = ('content')
+    list_display = ('title', 'slug', 'approved', 'created_on', 'location')
+    search_fields = ('title', 'content', 'location')
+    actions = ['approve_posts']
+
+    def approve_posts(self, request, queryset):
+        queryset.update(approved=True)
 
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
